4639c22c3f7d05632303dc8e25dfca630a141d71
Kenta Goto <kenta9736@gmail.com>
2024-05-27 23:00:39 +0900

    [追加]filterボタンの追加
    filterボタンの追加とCSSを少し変えました

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -4,14 +4,23 @@ import styles from "./Todo.module.css";
 
 export default function TodoComponent() {
     const [newTodo, setNewTodo] = useState("");
+    const [newPriority, setNewPriority] = useState("normal");
+    const [newProgress, setNewProgress] = useState("notDone");
+    const [priorityFilter, setPriorityFilter] = useState("all");
+    const [progressFilter, setProgressFilter] = useState("all");
 
     const [todos, setTodos] = useState([{
         id : "null",
         isDone: false,
         text: "サンプルタスク",
+        priority: "normal",
+        progress: "notDone",
     }]);
 
     const todoChange = (e) => setNewTodo(e.target.value);
+    const priorityChange = (e) => setNewPriority(e.target.value);
+    const priorityFilterChange = (e) => setPriorityFilter(e.target.value);
+    const progressChange = (e) => setProgressFilter(e.target.value);
 
     const addTodos = () => {
         if(newTodo ==="") return;
@@ -20,11 +29,14 @@ export default function TodoComponent() {
             id: new Date().getTime().toString(),
             isDone: false,
             text: newTodo,
+            priority: newPriority,
+            progress: newProgress,
         };
 
-        setTodos([...todos, newTodoItem
-        ]);
+        setTodos([...todos, newTodoItem]);
         setNewTodo("");
+        setNewPriority("normal");
+        setNewProgress('notDone');
     };
 
     useEffect(() => {
@@ -48,23 +60,67 @@ export default function TodoComponent() {
         localStorage.setItem("todolist", JSON.stringify(todos));
     }, [todos]);
 
+    const filteredTodos = () => {
+        if(priorityFilter === 'all' && progressFilter === "all"){
+            return todos;
+        }
+        else if(priorityFilter === 'all'){
+            return todos.filter(todo => todo.progress === progressFilter);
+        }
+        else if(progressFilter === "all"){
+            return todos.filter(todo => todo.priority === priorityFilter);
+        }
+        else{
+            return todos.filter(todo => todo.priority === priorityFilter).filter(td => td.progress === progressFilter);
+        }
+    }
+
+
     return (
         <div className={styles.box}>
-            <input className={styles.input} value={newTodo} onChange={todoChange}/>
-            <button className={styles.add} onClick={addTodos}>add</button>
+            <div className={styles.inputbox}>
+                <input id="inputbox"className={styles.input} value={newTodo} onChange={todoChange}/>
+                <select id="inputPriority"className={styles.select} value={newPriority} onChange={priorityChange}>
+                    <option value="emergency">emergency</option>
+                    <option value="important">important</option>
+                    <option value="normal">normal</option>
+                    <option value="trifle">trifle</option>
+                </select>
+                <button className={styles.add} onClick={addTodos}>add</button>
+            </div>
+            <div className={styles.priorityFilter} >
+                <p>priority filter</p>
+                <select id="prioritySelector" className={styles.priorirySlector} value={priorityFilter} onChange={priorityFilterChange}>
+                    <option value="all">all</option>
+                    <option value='emergency'>emergency</option>
+                    <option value="important">important</option>
+                    <option value="normal">normal</option>
+                    <option value="trifle">trifle</option>
+                </select>
+            </div>
+            <div id="progressFilter" className={styles.progressFilter} value={progressFilter} onChange={progressChange}>
+                <p>progress filter</p>
+                <select id ="progressSelector">
+                    <option value="all">all</option>
+                    <option value="done">done</option>
+                    <option value="notDone">not done</option>
+                </select>
+            </div>
             <ul className={styles.list}>
-                {todos.map((todo,index) => todo.id !== "null" ?(
+                {typeof filteredTodos().map === 'function' && filteredTodos().map((todo, index) => todo.id !== 'null' ?(
                     <div className={styles.todo} key={todo.id}>
-                        <span className={styles.index}>{index+1}.</span>
+                        <span className={styles.index}>{index +1}</span>
                         <span className={styles.text}>{todo.text}</span>
+                        <span className={styles.priority}>{todo.priority}</span>
                         <button className={styles.remove} onClick={() => {
-                            const newTodos = [...todos];
-                            newTodos.splice(index, 1);
-                            setTodos(newTodos)
+                            const newTodo = [ ...todos];
+                            newTodo.splice(index, 1);
+                            setTodos(newTodo)
                         }}>remove</button>
                     </div>
-                ): null)}
+                ):null)}
             </ul>
+            
         </div>
     );
 }
